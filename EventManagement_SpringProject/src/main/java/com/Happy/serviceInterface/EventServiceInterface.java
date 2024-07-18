7239e29a4e8f3706a34aaf98484ff965b49fb7a8
vandana <kadepallivandana2002@gmail.com>
2024-07-18 11:51:58 +0530

    EventManagement

---
+++ EventManagement_SpringProject/src/main/java/com/Happy/serviceInterface/EventServiceInterface.java
@@ -0,0 +1,20 @@
+package com.Happy.serviceInterface;
+
+
+
+import java.util.List;
+
+import com.Happy.entity.EventsEntity;
+import com.Happy.entity.Form;
+
+public interface EventServiceInterface {
+
+   // public String loadEvent(String eventCategory);
+   public EventsEntity loadEventData(String eventCategory);
+
+public List<Form> getUserEvents(String email);
+
+public void deleteUserBooking(Integer id);
+
+
+}
